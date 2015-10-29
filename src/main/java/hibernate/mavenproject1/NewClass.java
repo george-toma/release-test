b00ae0100a3c84abdce383cdff5e726fc2c88f5d
george-toma <toma.george89@yahoo.com>
2015-10-29 22:24:39 +0200

    Bug on  collectionID

---
+++ src/main/java/hibernate/mavenproject1/NewClass.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hibernate.mavenproject1;
+
+/**
+ *
+ * @author spykee
+ */
+public class NewClass {
+ final int a = 4;
+ 
+
+    public NewClass() {
+    }
+
+    public static void main(String[] args) {
+        {
+    int x =3;
+            System.out.println(++x*5/x--+--x);
+            short t = 10;
+            long a = t;
+        }
+    }
+
+}
